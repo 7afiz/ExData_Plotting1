2f3e75887c4e707957e0e716148e56fc628b4a17
7afiz <hafizmustafa76@gmail.com>
2014-08-08 20:39:41 +0300

    Add file description

--- plot1.R
+++ plot1.R
@@ -1,13 +1,22 @@
-# Load "household_power_consumption.txt" data into R
+# This file contains code for:
+#     1. Reading the data of Individual Household Electric Power Consumption Data Set from "household_power_consumption.txt" 
+#        File URL: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+#     2. merge and convert Date and Time columns into one column "full_time".
+#     3. subseet the data using only data from the dates 2007-02-01 and 2007-02-02.
+#     4. producing histogram graph  histogram for "Global_actve_power"
+#     5. and export the graph to plot1.png file.
+
+
+## Load "household_power_consumption.txt" data into R
 powerData <- read.table(file = "household_power_consumption.txt", 
                         header = TRUE, sep = ";", na.strings = "?",
                         nrows = 2075259, comment.char = "")
 
-# Merge and convert Date and Time columns into one column "full_time"
+## Merge and convert Date and Time columns into one column "full_time"
 powerData$Date_full <- strptime(paste(powerData$Date, powerData$Time), 
                                 "%d/%m/%Y%H:%M:%S")
 
-# Subseet powerData using only data from the dates 2007-02-01 and 2007-02-02.
+## Subseet powerData using only data from the dates 2007-02-01 and 2007-02-02.
 powerDataSubset <- subset(powerData, 
                           powerData$Date_full <= "2007-02-02 23:59:59" & 
                                 powerData$Date_full >= "2007-02-01 00:00:00", 
@@ -23,5 +32,5 @@ hist(powerDataSubset$Global_active_power,
      xlab = "Global Active Power (Kilowattts)", 
      ylab = "Frequency")
 
-# close graphic device (PNG)
-dev.off()
+# close to png file:
+dev.off() 
